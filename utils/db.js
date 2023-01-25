6ae5f5673e927bfbf62f88b9a2e49834d17e0bd2
Rijalul Akhri <109951346+rijalulakhri-dev@users.noreply.github.com>
2023-01-25 14:24:03 +0700

    Add files via upload

--- utils/db.js
+++ utils/db.js
@@ -1,1 +1,19 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/kampus', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+});
 
+// membuat schema
+
+
+// menambah satu data 
+// const contact1 = new Contact({
+//     nama: 'loki',
+//     nohp: '0827601322233',
+//     email: 'loki@gmail.com'
+// })
+
+// // simpan ke collection
+// contact1.save().then((contact) => console.log(contact))
